4e048b23e54e6e1710dd11ed6167565246a2b28a
Vinayak Bagaria <workvinayak95@gmail.com>
2021-06-15 22:10:10 +0530

    Cancel and show animation

--- src/Canvas/index.tsx
+++ src/Canvas/index.tsx
@@ -2,9 +2,6 @@ import { useEffect, useRef, useState } from 'react';
 import Scene from './Scene';
 import * as CanvasStyles from './styles';
 
-const WIDTH = 145;
-const HEIGHT = 185;
-
 const Canvas = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [currentIndex, setCurrentIndex] = useState(1);
@@ -30,16 +27,11 @@ const Canvas = () => {
         <CanvasStyles.Button
           type="button"
           onClick={handleButtonClick}
-          className="machine-button"
           aria-label="Throw a ball"
         />
       </CanvasStyles.ButtonWrapper>
       <CanvasStyles.Image src="vendingMachine.png" alt="Vending Machine" />
-      <CanvasStyles.CanvasElement
-        ref={canvasRef}
-        width={WIDTH}
-        height={HEIGHT}
-      />
+      <CanvasStyles.CanvasElement ref={canvasRef} />
     </CanvasStyles.Wrapper>
   );
 };
